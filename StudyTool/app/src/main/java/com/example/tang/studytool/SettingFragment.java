8db0a4753d1a14677e66d0f670ae76a7f02f04ea
your 2520TANGBO <1263656881@qq.com>
2017-01-12 10:17:31 +0800

    添加删除计划完成
    采用控件Recyclerview、CheckBox、ButterKnife快速注入、SwipeRefreshLayout、EditText、Button、ImageView、Fragment、TimePickerDialog、Handler异步调用、Toolbar等控件。
    以及自定义View，sqlite数据库。
    
    （1）读取数据库以及显示：显示用户之前的计划
    （2）新建新计划：让用户随时新建计划
    （3）修改计划：计划内容不当,可以修改
    （4）完成计划：完成计划
    （5）删除计划：不需要的计划可以删除
    （6）刷新计划：刷新计划
    （7）番茄工作法：用户督促完成任务
    附录1：用户手册
    1、 单击任务修改
    2、 菜单新建计划
    3、 下拉刷新
    4、 复选框单击代表完成任务
    5、 单击时间选择倒计时时间
    6、 开始按钮开始倒计时
    7、 结束按钮结束倒计时
    8、 单击选项卡切换页面

--- StudyTool/app/src/main/java/com/example/tang/studytool/SettingFragment.java
+++ StudyTool/app/src/main/java/com/example/tang/studytool/SettingFragment.java
@@ -1,14 +1,11 @@
-package com.example.tang.studytool.TabFragment;
+package com.example.tang.studytool;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.tang.studytool.R;
 
 /**
  * Created by Tang on 2016/12/24.
